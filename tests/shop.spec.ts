35f6f87e67fa993f9dcec3c2a95ebbaeb367876b
Ramon N. Gene <rgene@rccl.com>
2026-02-16 12:59:33 -0500

    deleted shop.spec

--- tests/shop.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('homepage shows all products', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.getByText('Demo Store')).toBeVisible();
-  await expect(page.getByTestId('product-1')).toBeVisible();
-  await expect(page.getByTestId('product-6')).toBeVisible();
-});
