5fb05e62128d0b491e16f529ae3b4213f83cf92b
Ramon N. Gene <rgene@rccl.com>
2026-02-16 12:56:38 -0500

    Lowered timeouts

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,8 +4,10 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: process.env.CI ? 0 : 0,
+  workers: process.env.CI ? 0 : undefined,
+  timeout: 3_000,
+  expect: { timeout: 3_000 },
   reporter: 'html',
   use: {
     baseURL: 'http://localhost:5173',
